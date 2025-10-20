f2b328d148939b2cb3918caa8106b0acad1198df
Emerick1009 <emerckorlando@gmail.com>
2025-10-19 21:52:16 -0600

    Día 5 - Gestor de contraseñas versión 1.0

--- gestor.py
+++ gestor.py
@@ -1,3 +1,5 @@
+import os
+
 def guardar_contraseña(nombre, contraseña, archivo="contraseñas_guardadas.txt"):
     with open(archivo, "a") as f:
         f.write(f"{nombre}: {contraseña}\n")
@@ -14,3 +16,51 @@ def ver_contraseñas(archivo="contraseñas_guardadas.txt"):
                     print(linea.strip())
     except FileNotFoundError:
         print("No se encontró el archivo de contraseñas.")
+
+def buscar_contraseña(nombre, archivo="contraseñas_guardadas.txt"):
+    try:
+        with open(archivo,"r") as f:
+            for linea in f:
+                if linea.lower().startswith(nombre.lower() + ":"):
+                    print(f"{linea.strip()}")
+                    return
+            print("No se encontró esa contraseña")
+    except FileNotFoundError:
+        print("No se encontro el archivo de contraseñas")
+
+def eliminar_contraseña(nombre, archivo="contraseñas_guardadas.txt"):
+    try:
+        with open(archivo, "r") as f:
+            lineas = f.readlines()
+        with open(archivo, "w") as f:
+            eliminada = False
+            for linea in lineas: 
+                if not linea.lower().startswith(nombre.lower() + ":"):
+                    f.write(linea)
+                else:
+                    eliminada = True
+                if eliminada:
+                    print(f"Contraseña de {nombre} eliminada.")
+                else:
+                    print("No se encontró esa contraseña.")
+    except FileNotFoundError:
+        print("No hay contraseñas guardadas aun.")
+
+def actualizar_contraseña(nombre,nueva, archivo="contraseñas_guardadas.txt"):
+    try:
+        with open(archivo, "r") as f:
+            lineas = f.readlines()
+        with open(archivo, "w") as f:
+            actualizada = False
+            for linea in lineas:
+                if linea.lower().startswith(nombre.lower() + ":"):
+                    f.write(f"{nombre}: {nueva}\n")
+                    actualizada = True
+                else:
+                    f.write(linea)
+            if actualizada:
+                print(f"Contraseña de {nombre} actualizada.")
+            else:
+                print("No se encontro esa contraseña")
+    except FileNotFoundError:
+        print("No hay contraseñas guardadas aun.")
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from generador import generar_contraseña
-from gestor import guardar_contraseña, ver_contraseñas
+from gestor import (
+    guardar_contraseña, ver_contraseñas,
+    buscar_contraseña, eliminar_contraseña,
+    actualizar_contraseña
+    )
 
 def menu():
     while True:
         print("\n=== Gestor de Contraseñas ===")
         print("1. Generar y guardar nueva contraseña")
         print("2. Ver contraseñas guardadas")
-        print("3. Salir")
+        print("3. Busar una contraseña")
+        print("4. Eliminar una contraseña")
+        print("5. Actualizar contraseña")
+        print("6. Salir")
 
         opcion = input("Elige una opción: ")
 
@@ -19,6 +26,16 @@ def menu():
         elif opcion == "2":
             ver_contraseñas()
         elif opcion == "3":
+            nombre = input("Introduce eñ servicio a buscar: ")
+            buscar_contraseña(nombre)
+        elif opcion == "4":
+            nombre = input("Introduce el servicio a eliminar: ")
+            eliminar_contraseña(nombre)
+        elif opcion == "5":
+            nombre = input("Introduce el servicio a actualizar: ")
+            nueva = input("Escribe la nueva contraseña: ")
+            actualizar_contraseña(nombre, nueva)
+        elif opcion == "6":
             print("Saliendo del gestor... ¡Hasta luego!")
             break
         else:
